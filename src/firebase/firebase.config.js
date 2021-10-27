afde6940324f49b01f775c4c956d0ab31bad19be
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-27 21:16:12 +0600

    first-commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsWZR23ksJuWdLMyhYOQ9QISFrximefK8",
+  authDomain: "email-password-authentic-8dbab.firebaseapp.com",
+  projectId: "email-password-authentic-8dbab",
+  storageBucket: "email-password-authentic-8dbab.appspot.com",
+  messagingSenderId: "753046225299",
+  appId: "1:753046225299:web:7d0df3f3b83b66c76ae866",
+};
+export default firebaseConfig;
