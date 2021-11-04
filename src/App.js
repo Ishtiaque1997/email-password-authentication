3ce57ee29b8d148a8265740d4a245549d982918b
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-11-04 12:37:34 +0600

    create user done

--- src/App.js
+++ src/App.js
@@ -16,50 +16,61 @@ function App() {
      const user=res.user;
      console.log(user)
     })
+}
 
-  }
+
+
+
   
  const handleRegistration=(e)=>{
    e.preventDefault();
+   createUserWithEmailAndPassword(auth,email,password)
+   .then(res=>{
+     const user=res.user;
+     console.log(user)
+   })
    console.log(email,password)
+}
 
- }
 const handleEmailChange=e=>{
   setEmail(e.target.value);
 }
+
 const handlePasswordChange=e=>{
   setPassword(e.target.value);
 }
+
+
   
   
   return (
     <div className='mx-5'>
      <form onSubmit={handleRegistration}>
        <h3 className='text-primary'>Please register</h3>
-  <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input onChange={handleEmailChange} type="email" class="form-control" id="inputEmail3"required/>
+  <div className="row mb-3">
+    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+    <div className="col-sm-10">
+      <input onChange={handleEmailChange} type="email" className="form-control" id="inputEmail3"required/>
     </div>
   </div>
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input onChange={handlePasswordChange} type="password" class="form-control" id="inputPassword3"required/>
+  <div className="row mb-3">
+    <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+    <div className="col-sm-10">
+      <input onChange={handlePasswordChange} type="password" className="form-control" id="inputPassword3"required/>
     </div>
   </div>
   
-  <div class="row mb-3">
-    <div class="col-sm-10 offset-sm-2">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck1"/>
-        <label class="form-check-label" for="gridCheck1">
+  <div className="row mb-3">
+    <div className="col-sm-10 offset-sm-2">
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" id="gridCheck1"/>
+        <label className="form-check-label" htmlFor="gridCheck1">
           Example checkbox
         </label>
       </div>
     </div>
   </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
+  <button type="submit" className="btn btn-primary">Register</button>
 </form>
       <br /><br /><br />
     <div>--------------</div>
